b778ecd51af380ca44b8d16701f10ddd6c211dd4
Arthur Kenfack <arthurkenfack00@gmail.com>
2025-03-01 09:12:11 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/winestockmanagementsystem/Winestockmanagementsystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.winestockmanagementsystem;
+
+import interfaces.LoginPage;
+
+/**
+ *
+ * @author Mr Creepy
+ */
+public class Winestockmanagementsystem {
+
+    public static void main(String[] args) {
+       LoginPage login = new LoginPage();
+       login.setVisible(true);
+    }
+}
